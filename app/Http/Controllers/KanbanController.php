b5b4f77125299e6daeee669b7e276acc9c45f380
Margulan <margulan@0x0.kz>
2025-03-03 23:06:32 +0500

    kanban in process

--- app/Http/Controllers/KanbanController.php
+++ app/Http/Controllers/KanbanController.php
@@ -12,42 +12,41 @@ class KanbanController extends Controller
     public function interact(Request $request)
     {
 
-        if ($request->isMethod('get')) {
+        if ($request->isMethod('post')) {
 
-            return view('kanban');
+            $data = $request->validate(
+                [
+                    'title' => 'required|string',
+                    'text' => 'required|string',
+                    'priority' => 'required|string',
+                    // 'va1' => 'required|string',
+                    // 'va2' => 'required|string',
+                    // 'va3' => 'required|string',
+                ],
+                [
+                    'title.required' => 'Title is required',
+                    'text.required' => 'Text is required',
+                    'priority.required' => 'Priority is required',
+                    // 'va1.required' => 'Va1 is required',
+                    // 'va2.required' => 'Va2 is required',
+                    // 'va3.required' => 'Va3 is required',
+                ]
+            );
+
+            $data['column'] = 'todo';
+
+            if (Auth::check()) {
+                $data['author'] = Auth::user()->name;
+            } else {
+                $data['author'] = 'Guest';
+            }
+
+            Kanban::create($data);
+
         }
 
-        $data = $request->validate(
-            [
-                'title' => 'required|string',
-                'text' => 'required|string',
-                'priority' => 'required|string',
-                // 'va1' => 'required|string',
-                // 'va2' => 'required|string',
-                // 'va3' => 'required|string',
-            ],
-            [
-                'title.required' => 'Title is required',
-                'text.required' => 'Text is required',
-                'priority.required' => 'Priority is required',
-                // 'va1.required' => 'Va1 is required',
-                // 'va2.required' => 'Va2 is required',
-                // 'va3.required' => 'Va3 is required',
-            ]
-        );
+        $kanban = Kanban::all();
 
-        $data['column'] = 'todo';
-
-        if(Auth::check()) {
-            $data['author'] = Auth::user()->name;
-        } else {
-            $data['author'] = 'Guest';
-        }
-
-        // $data = $request->all();
-
-        Kanban::create($data);
-
-        return view('kanban');
+        return view('kanban', compact('kanban'));
     }
 }
--- resources/views/kanban.blade.php
+++ resources/views/kanban.blade.php
@@ -5,10 +5,11 @@
 @section('content')
 
 
-  @if (isset($data))
+  @if (isset($kanban))
     @php
       echo '<pre>';
-      print_r($data);
+      // print_r($kanban->count());
+      // print_r($kanban);
       echo '</pre>';
     @endphp
   @endif
@@ -21,143 +22,114 @@
     @endphp
   @endif
 
-  <h1 class="mx-auto text-2xl my-3 px-20 pb-2 bg-gradient-to-r from-transparent via-gray-900 to-transparent">В
-    разработке...</h1>
+  <div class="flex flex-col my-3 text-neutral-200">
 
-  <div class="flex flex-wrap justify-center gap-5 text-neutral-900 pb-5">
+    <h1 class="w-85 text-lg rounded-2xl my-3 py-3 bg-neutral-800 px-5">Канбан (в разработке...)</h1>
 
-    <div class="todo w-85 border-t-6 border-orange-300 bg-white px-3">
+    <div class="flex flex-wrap rounded-2xl gap-3">
+      <div class="todo w-85 h-max px-3 bg-neutral-800 rounded-2xl">
+        <div class="header h-18 flex flex-col justify-center">
+          <span class="font-semibold text-lg ml-5">План ({{ $kanban->count() }})</span>
+        </div>
+        @if (isset($kanban))
+          @foreach ($kanban as $item)
+            <div class="border-1 border-neutral-300 rounded-2xl mb-5">
+              <div class="p-4">
+                <h2
+                  class="w-max mb-2 rounded-full px-4 py-1 {{ match ($item->priority) {
+                      'high' => 'bg-rose-700',
+                      'low' => 'bg-emerald-700',
+                      'medium' => 'bg-fuchsia-700',
+                  } }}">
+                  {{ $item->title }}</h2>
+                <p class="indent-3">
+                  {{ $item->text }}
+                </p>
+              </div>
+              <div class="flex justify-end cursor-pointer">
+                <div class="px-5 cursor-pointer rounded-br-full">To the Right</div>
+              </div>
+            </div>
+          @endforeach
+        @endif
 
-      <div class="header h-18 flex flex-col justify-center items-center">
-        <span class="font-semibold text-lg">To Do</span>
-        <span class="text-sm">5 cards</span>
-      </div>
+        {{-- Input Form --}}
 
-      <div class="border-1 border-neutral-300 bg-neutral-100 text-sm text-neutral-700 border-t-4 border-t-rose-300 mb-5">
-        <p class="indent-3 px-3 my-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure perferendis
-          consectetur,
-          sunt, eum delectus
-          alias facere natus harum iusto quasi voluptatum ut eius aspernatur, quibusdam veritatis qui quo itaque illo.
-        </p>
-        <div class="flex justify-end cursor-pointer">
-          <div class="bg-neutral-800 text-white px-5 cursor-pointer">To the Right</div>
+        <div class="border-1 border-neutral-300 rounded-2xl mb-5 px-3 p-4">
+          <form action="{{ route('kanban') }}" method="post">
+            @csrf
+            <p class="py-3 font-semibold">Новый Стикер</p>
+            <input type="text" name="title" class="w-full rounded-md border-1 p-2 mb-3" placeholder="Заголовок">
+            <textarea name="text" rows="4" class="w-full rounded-md border-1 p-2 mb-3" placeholder="Текст"></textarea>
+            <select name="priority" class="w-full bg-neutral-800 rounded-md border-1 p-2 mb-3">
+              <option value="high" class="text-red-500">Высокий приоритет</option>
+              <option value="medium" class="text-neutral-300" selected>Без приоритета</option>
+              <option value="low" class="text-green-500">Низкий приоритет</option>
+            </select>
+            <button type="submit" class="py-1 px-5 border-1 rounded-md">Добавить</button>
+          </form>
         </div>
       </div>
 
-      <div
-        class="border-1 border-neutral-300 bg-neutral-100 text-sm text-neutral-700 border-t-4 border-t-emerald-300 mb-5">
-        <p class="indent-3 px-3 my-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure perferendis
-          consectetur,
-          sunt, eum delectus
-          alias facere natus harum iusto quasi voluptatum ut eius aspernatur, quibusdam veritatis qui quo itaque illo.
-        </p>
-        <div class="flex justify-end">
-          <div class="bg-neutral-800 text-white px-5 cursor-pointer">To the Right</div>
+      {{-- Divider --}}
+
+      <div class="todo w-85 h-max px-3 bg-neutral-800 rounded-2xl">
+        <div class="header h-18 flex flex-col justify-center">
+          <span class="font-semibold text-lg ml-5">В Процессе ({{ $kanban->count() }})</span>
         </div>
+        @if (isset($kanban))
+          @foreach ($kanban as $item)
+            <div class="border-1 border-neutral-300 rounded-2xl mb-5">
+              <div class="p-4">
+                <h2
+                  class="w-max mb-2 rounded-full px-4 py-1 {{ match ($item->priority) {
+                      'high' => 'bg-rose-700',
+                      'low' => 'bg-emerald-700',
+                      'medium' => 'bg-fuchsia-700',
+                  } }}">
+                  {{ $item->title }}</h2>
+                <p class="indent-3">
+                  {{ $item->text }}
+                </p>
+              </div>
+              <div class="flex justify-end cursor-pointer">
+                <div class="px-5 cursor-pointer rounded-br-full">To the Right</div>
+              </div>
+            </div>
+          @endforeach
+        @endif
       </div>
 
-      <div
-        class="border-1 border-neutral-300 bg-neutral-100 text-sm text-neutral-700 border-t-4 border-t-emerald-300 mb-5">
-        <p class="indent-3 px-3 my-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure perferendis
-          consectetur,
-          sunt, eum delectus
-          alias facere natus harum iusto quasi voluptatum ut eius aspernatur, quibusdam veritatis qui quo itaque illo.
-        </p>
-        <div class="flex justify-end">
-          <div class="bg-neutral-800 text-white px-5 cursor-pointer">To the Right</div>
+      {{-- Divider --}}
+
+      <div class="todo w-85 h-max px-3 bg-neutral-800 rounded-2xl">
+        <div class="header h-18 flex flex-col justify-center">
+          <span class="font-semibold text-lg ml-5">Тест ({{ $kanban->count() }})</span>
         </div>
-      </div>
-
-      {{-- Input Form --}}
-
-      <div class="border-1 border-neutral-300 bg-neutral-100 border-t-4 px-3 p-4 text-sm text-neutral-700">
-        <form action="{{ route('kanban') }}" method="post">
-          @csrf
-          <p class="py-3 font-semibold">New Sticker</p>
-          <input type="text" name="title" class="w-full bg-white border-1 border-neutral-300 p-2 mb-3"
-            placeholder="Title">
-          <textarea name="text" rows="4" class="w-full bg-white border-1 border-neutral-300 p-2 mb-3" placeholder="Text"></textarea>
-          <select name="priority" class="w-full bg-white border-1 border-neutral-300 p-2 mb-3">
-            <option value="high" class="text-red-500">High Priority</option>
-            <option value="medium" class="text-emerald-500" selected>No Priority</option>
-            <option value="low" class="text-green-500">Low Priority</option>
-          </select>
-          <button type="submit" class="bg-neutral-800 text-white py-2 px-5 ">Add</button>
-        </form>
-      </div>
-    </div>
-
-    {{-- Divider --}}
-
-    <div class="inProcess w-85 border-t-6 border-red-400 bg-white px-3">
-
-      <div class="header h-18 flex flex-col justify-center items-center">
-        <span class="font-semibold text-lg">In Process</span>
-        <span class="text-sm">2 cards</span>
-      </div>
-
-      <div class="border-1 border-neutral-300 bg-neutral-100 text-sm text-neutral-700 border-t-4 border-t-rose-300 mb-5">
-        <p class="indent-3 px-3 my-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure perferendis
-          consectetur,
-          sunt, eum delectus
-          alias facere natus harum iusto quasi voluptatum ut eius aspernatur, quibusdam veritatis qui quo itaque illo.
-        </p>
-        <div class="flex justify-between">
-          <div class="bg-neutral-800 text-white px-5 cursor-pointer">To the Left</div>
-          <div class="bg-neutral-800 text-white px-5 cursor-pointer">To the Right</div>
-        </div>
-      </div>
-
-    </div>
-
-    {{-- Divider --}}
-
-    <div class="testing w-85 border-t-6 border-yellow-300 bg-white px-3">
-
-      <div class="header h-18 flex flex-col justify-center items-center">
-        <span class="font-semibold text-lg">Testing</span>
-        <span class="text-sm">3 cards</span>
-      </div>
-
-      <div class="border-1 border-neutral-300 bg-neutral-100 text-sm text-neutral-700 border-t-4 border-t-rose-300 mb-5">
-        <p class="indent-3 px-3 my-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure perferendis
-          consectetur,
-          sunt, eum delectus
-          alias facere natus harum iusto quasi voluptatum ut eius aspernatur, quibusdam veritatis qui quo itaque illo.
-        </p>
-        <div class="flex justify-between">
-          <div class="bg-neutral-800 text-white px-5 cursor-pointer">To the Left</div>
-          <div class="bg-neutral-800 text-white px-5 cursor-pointer">To the Right</div>
-        </div>
+        @if (isset($kanban))
+          @foreach ($kanban as $item)
+            <div class="border-1 border-neutral-300 rounded-2xl mb-5">
+              <div class="p-4">
+                <h2
+                  class="w-max mb-2 rounded-full px-4 py-1 {{ match ($item->priority) {
+                      'high' => 'bg-rose-700',
+                      'low' => 'bg-emerald-700',
+                      'medium' => 'bg-fuchsia-700',
+                  } }}">
+                  {{ $item->title }}</h2>
+                <p class="indent-3">
+                  {{ $item->text }}
+                </p>
+              </div>
+              <div class="flex justify-end cursor-pointer">
+                <div class="px-5 cursor-pointer rounded-br-full">To the Right</div>
+              </div>
+            </div>
+          @endforeach
+        @endif
       </div>
 
     </div>
-
-    {{-- Divider --}}
-
-    <div class="ready w-85 border-t-6 border-green-300 bg-white px-3">
-
-      <div class="header h-18 flex flex-col justify-center items-center">
-        <span class="font-semibold text-lg">Ready</span>
-        <span class="text-sm">2 cards</span>
-      </div>
-
-      <div class="border-1 border-neutral-300 bg-neutral-100 text-sm text-neutral-700 border-t-4 border-t-rose-300 mb-5">
-        <p class="indent-3 px-3 my-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure perferendis
-          consectetur,
-          sunt, eum delectus
-          alias facere natus harum iusto quasi voluptatum ut eius aspernatur, quibusdam veritatis qui quo itaque illo.
-        </p>
-        <div class="flex justify-between">
-          <div class="bg-neutral-800 text-white px-5 cursor-pointer">To the Left</div>
-          <div class="bg-neutral-800 text-white px-5 cursor-pointer">To the Right</div>
-        </div>
-      </div>
-
-    </div>
-
-
-
 
   </div>
 
